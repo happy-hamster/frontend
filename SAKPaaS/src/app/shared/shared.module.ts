84ce4b2f02b2893e6356c75bef976f7dec7df335
Niclas Kaufmann <niclaskau@web.de>
2020-03-20 22:28:37 +0100

    Created SharedModule

---
+++ SAKPaaS/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+  ],
+  exports: [
+    CommonModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+
+  ],
+})
+export class AppModule { }
