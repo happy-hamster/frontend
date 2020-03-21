4718a8896fd675e79007493a3c906aea91f485a0
patrickstecker <Stecker2000@gmail.com>
2020-03-21 21:57:45 +0100

    Shit code

---
+++ SAKPaaS/src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {Location} from "../../generated/models/location";
+import {LocationProviderService} from "../../core/services/location-provider.service";
+import {filter, map, startWith, switchMap, tap} from "rxjs/operators";
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss']
+})
+export class SearchBarComponent implements OnInit {
+  searchControl = new FormControl();
+  locations$: Observable<Location[]>;
+  filteredLocations$: Observable<Location[]>;
+
+  constructor(private locationsService: LocationProviderService) {
+    this.locations$ = locationsService.fetchLocations(5000);
+    this.filteredLocations$ = this.searchControl.valueChanges.pipe(
+      switchMap(value => {
+        console.log("1"+value);
+        if (value == '') {
+          return this.locations$;
+        }
+        value = value.toLowerCase();
+        return this.locations$.pipe(
+          tap(e => console.log("e"+e)),
+          map(locations => locations.filter(
+            location => location.name.toLowerCase().includes(value)
+          )),
+          tap(e => console.log(e))
+        )
+      })
+    )
+  }
+
+  ngOnInit(): void {
+    this.filteredLocations$.subscribe(item => {console.log("log: "+item)});
+  }
+}
