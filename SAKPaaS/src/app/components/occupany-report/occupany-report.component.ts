689ff9a9b2c213f537effd32dd6be00cd1cf4802
Niclas Kaufmann <niclaskau@web.de>
2020-03-21 13:46:51 +0100

    tmp commit

--- SAKPaaS/src/app/components/occupany-report/occupany-report.component.ts
+++ SAKPaaS/src/app/components/occupany-report/occupany-report.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Location } from 'src/app/generated/models';
+import { switchMap } from 'rxjs/operators';
+import { LocationProviderService } from 'src/app/core/services/location-provider.service';
 
 @Component({
   selector: 'app-occupany-report',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OccupanyReportComponent implements OnInit {
 
-  constructor() { }
+  selectedLocation$: Observable<Location>;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private locationService: LocationProviderService
+  ) { }
 
   ngOnInit(): void {
+    this.selectedLocation$ = this.activatedRoute.params.pipe(
+      switchMap(params => {
+        if (!params.id) {
+          return undefined;
+        }
+        return this.locationService.fetchLocationById(params.id);
+      })
+    );
+
+    this.selectedLocation$.subscribe(location => console.log(location));
   }
 
 }
