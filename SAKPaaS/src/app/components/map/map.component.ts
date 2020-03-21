c822ecd9f1a42bfbbe51d4b36b4c3553e498d214
timgrohmann <timgrohmann@icloud.com>
2020-03-21 13:35:46 +0100

    fixed map styling

--- SAKPaaS/src/app/components/map/map.component.ts
+++ SAKPaaS/src/app/components/map/map.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
+import { OSM } from 'ol/source';
 import * as olProj from 'ol/proj';
+import { defaults as defaultInteractions, Translate } from 'ol/interaction';
 import { GpsService } from 'src/app/core/services/gps.service';
 
 @Component({
@@ -17,25 +18,29 @@ export class MapComponent implements OnInit {
 
   constructor(
     private gpsService: GpsService,
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
-    this.gpsService.getLocation().subscribe(gpsCoordinates => {
-      console.log("Map will be loaded")
-      this.customMap = new Map({
-        target: 'map',
-        layers: [
-          new TileLayer({
-            source: new XYZ({
-              url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            })
-          })
-        ],
-        view: new View({
-          center: olProj.fromLonLat([gpsCoordinates.longitude, gpsCoordinates.latitude]),
-          zoom: 2
+    this.customMap = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
         })
-      });
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      }),
+    });
+
+    console.log(this.customMap.getInteractions())
+
+    this.gpsService.getLocation().subscribe(gpsCoordinates => {
+      console.log("Setting map center...")
+      this.customMap.getView().setCenter(olProj.fromLonLat([gpsCoordinates.longitude, gpsCoordinates.latitude]));
+      this.customMap.getView().setZoom(10);
     });
   }
 }
