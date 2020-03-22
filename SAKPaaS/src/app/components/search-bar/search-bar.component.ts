b4c6877dfba92423740b3505af326c7c5b8cc060
Joost <MrKraboom@outlook.com>
2020-03-22 01:19:25 +0100

    Fixed search a bit

--- SAKPaaS/src/app/components/search-bar/search-bar.component.ts
+++ SAKPaaS/src/app/components/search-bar/search-bar.component.ts
@@ -3,7 +3,7 @@ import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {Location} from '../../generated/models/location';
 import {LocationProviderService} from '../../core/services/location-provider.service';
-import {map, switchMap, tap} from 'rxjs/operators';
+import {map, switchMap, tap, concatMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-bar',
@@ -12,22 +12,19 @@ import {map, switchMap, tap} from 'rxjs/operators';
 })
 export class SearchBarComponent implements OnInit {
   searchControl = new FormControl();
-  private locations: Location[];
   filteredLocations$: Observable<Location[]>;
 
   constructor(private locationsService: LocationProviderService) {}
 
   ngOnInit(): void {
-    this.locationsService.fetchLocations(5000)
-      .subscribe(locations => this.locations = locations);
-
-    this.filteredLocations$ = this.searchControl.valueChanges.pipe(
-      map(value => {
-        value = value.toLowerCase();
-        return this.locations.filter(
-          location => location.name.toLowerCase().includes(value)
-        );
-      })
+    this.filteredLocations$ = this.locationsService.fetchLocations(5000)
+      .pipe(
+        switchMap(locations => this.searchControl.valueChanges.pipe(
+          map(value => {
+            return locations.filter(location => location.name.toLowerCase().includes(value));
+          })
+        )
+      )
     );
   }
 }
