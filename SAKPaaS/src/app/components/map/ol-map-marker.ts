39feb249a5a9a8ca11700681ba859b9b483a07d1
timgrohmann <timgrohmann@icloud.com>
2020-03-22 13:54:59 +0100

    added marker colors

--- SAKPaaS/src/app/components/map/ol-map-marker.ts
+++ SAKPaaS/src/app/components/map/ol-map-marker.ts
@@ -7,33 +7,54 @@ import { Location } from 'src/app/generated/models';
 
 export class OLMapMarker extends Feature {
 
-  public location: Location
 
-  constructor(locationMarker: Location) {
-    super({
-      geometry: new Point(olProj.fromLonLat([locationMarker.longitude, locationMarker.latitude])),
-    });
-    this.location = locationMarker
-    this.setStyle(OLMapMarker.markerStyle)
-  }
-
-  static markerStyle: Style = new Style({
-    image: new Icon({
-      anchor: [0.5, 1],
-      anchorXUnits: IconAnchorUnits.FRACTION,
-      anchorYUnits: IconAnchorUnits.FRACTION,
-      src: "assets/icons/marker.png",
-      scale: 0.2
-    })
-  })
+  static markerStyleUndefined: Style = OLMapMarker.markerStyleMaker('marker_0');
+  static markerStyleEmpty: Style = OLMapMarker.markerStyleMaker('marker_1');
+  static markerStyleMedium: Style = OLMapMarker.markerStyleMaker('marker_2');
+  static markerStyleFull: Style = OLMapMarker.markerStyleMaker('marker_3');
 
   static markerStyleSelected: Style = new Style({
     image: new Icon({
       anchor: [0.5, 1],
       anchorXUnits: IconAnchorUnits.FRACTION,
       anchorYUnits: IconAnchorUnits.FRACTION,
-      src: "assets/icons/marker.png",
+      src: 'assets/icons/marker.png',
       scale: 0.25
     })
-  })
+  });
+
+  public location: Location;
+
+  constructor(locationMarker: Location) {
+    super({
+      geometry: new Point(olProj.fromLonLat([locationMarker.longitude, locationMarker.latitude])),
+    });
+    this.location = locationMarker;
+    const occupancy = locationMarker.occupancy;
+    if (occupancy == null || occupancy < 0) {
+      this.setStyle(OLMapMarker.markerStyleUndefined);
+    } else {
+      const oc3 = occupancy * 3;
+      if (oc3 < 1) {
+        this.setStyle(OLMapMarker.markerStyleEmpty);
+      } else if (oc3 < 2) {
+        this.setStyle(OLMapMarker.markerStyleMedium);
+      } else {
+        this.setStyle(OLMapMarker.markerStyleFull);
+      }
+    }
+  }
+
+  static markerStyleMaker(iconName: string): Style {
+    return new Style({
+      image: new Icon({
+        anchor: [0.5, 1],
+        anchorXUnits: IconAnchorUnits.FRACTION,
+        anchorYUnits: IconAnchorUnits.FRACTION,
+        src: 'assets/icons/marker/' + iconName + '.png',
+        scale: 0.15
+      })
+    });
+  }
+
 }
