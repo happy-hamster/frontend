141ef0e877722db19638fb54849807cc0a30c9dc
Niclas Kaufmann <niclaskau@web.de>
2020-03-23 09:42:43 +0100

    Removed test route

--- SAKPaaS/src/app/app-routing.module.ts
+++ SAKPaaS/src/app/app-routing.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
-import {LocationDetailsComponent} from './components/location-details/location-details.component';
+import { HomeComponent } from './components/home/home.component';
 import { OccupancyReportComponent } from 'src/app/components/occupancy-report/occupancy-report.component';
-import {ImpressumComponent} from "./components/impressum/impressum.component";
-
-
+import { ImpressumComponent } from './components/impressum/impressum.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', redirectTo: ''},
   {path: 'reportOccupancy/:id', component: OccupancyReportComponent},
-  {path: 'test', component: LocationDetailsComponent},
   {path: 'impressum', component: ImpressumComponent},
   {path: '**', redirectTo: ''}
 ];
