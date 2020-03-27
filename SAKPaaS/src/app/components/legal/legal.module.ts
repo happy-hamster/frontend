1c3c58cff27ef0c0c5ea0feb2a0c63ed825566f9
Niclas Kaufmann <niclaskau@web.de>
2020-03-27 22:19:12 +0100

    Added legal component

---
+++ SAKPaaS/src/app/components/legal/legal.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LegalComponent } from './legal.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { ImpressumComponent } from 'src/app/components/legal/impressum/impressum.component';
+import { LegalRoutingModule } from 'src/app/components/legal/legal-routing.module';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+// lazy loaded
+
+@NgModule({
+  declarations: [
+    LegalComponent,
+    PrivacyPolicyComponent,
+    ImpressumComponent
+  ],
+  imports: [
+    CommonModule,
+    LegalRoutingModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class LegalModule { }
