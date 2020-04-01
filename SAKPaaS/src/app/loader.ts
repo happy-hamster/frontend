db8dd403c941affd8898891bc430c6603b038a80
Niclas Kaufmann <niclaskau@web.de>
2020-04-01 17:13:18 +0200

    Created Loader function

---
+++ SAKPaaS/src/app/loader.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable, ObservableInput, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { ApiConfiguration } from 'src/app/generated/api-configuration';
+
+export function load(http: HttpClient, config: ApiConfiguration): (() => Promise<boolean>) {
+  return (): Promise<boolean> => {
+    return new Promise<boolean>((resolve: (a: boolean) => void): void => {
+       http.get('./config.json')
+         .pipe(
+           map((x: any) => {
+             config.rootUrl = x.api_rootUrl;
+             resolve(true);
+           }),
+           catchError((x: { status: number }, caught: Observable<void>): ObservableInput<{}> => {
+             if (x.status !== 404) {
+               resolve(false);
+             }
+             config.rootUrl = 'https://api.happyhamster.org/v1';
+             resolve(true);
+             return of({});
+           })
+         ).subscribe();
+    });
+  };
+}
