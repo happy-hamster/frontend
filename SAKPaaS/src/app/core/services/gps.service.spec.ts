34d2c4f482e03f3126df7a4b4686c2edc8806d27
Niclas Kaufmann <niclaskau@web.de>
2020-04-13 18:33:10 +0200

    Updated tests for services

--- SAKPaaS/src/app/core/services/gps.service.spec.ts
+++ SAKPaaS/src/app/core/services/gps.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GpsService } from './gps.service';
+import { MatDialogTestingModule } from 'src/app/shared/mat-dialog-testing.module';
 
 describe('GpsService', () => {
   let service: GpsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        MatDialogTestingModule
+      ]
+    });
     service = TestBed.inject(GpsService);
   });
 
--- SAKPaaS/src/app/core/services/location-provider.service.spec.ts
+++ SAKPaaS/src/app/core/services/location-provider.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LocationProviderService } from './location-provider.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogTestingModule } from 'src/app/shared/mat-dialog-testing.module';
 
 describe('LocationProviderService', () => {
   let service: LocationProviderService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        MatDialogTestingModule
+      ]
+    });
     service = TestBed.inject(LocationProviderService);
   });
 
