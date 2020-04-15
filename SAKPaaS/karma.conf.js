a2f16635afd3ab662fd3eb35496a109d35a414bd
RobSpot-Gaming <35638696+RobSpot-Gaming@users.noreply.github.com>
2020-04-15 10:46:40 +0200

    Rollback of Karma.conf.ts

--- SAKPaaS/karma.conf.js
+++ SAKPaaS/karma.conf.js
@@ -1,7 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -29,11 +28,5 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
-    customLaunchers: {
-      ChromeHeadlessCustom: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
-  },
-},
   });
 };
