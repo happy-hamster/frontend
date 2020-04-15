4a20bfda995af017b84ce26f52cf47fdf26cf9b9
Unknown <lepathi@gmail.com>
2020-04-15 12:55:03 +0200

    Added lifecycle-hook
    and moved logic to it.

--- SAKPaaS/src/app/app.component.ts
+++ SAKPaaS/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, AfterContentInit } from '@angular/core';
 import { LocationProviderService } from './core/services/location-provider.service';
 import { Observable } from 'rxjs';
 import { Location } from 'src/app/generated/models/location';
@@ -6,7 +6,6 @@ import { TranslateService } from '@ngx-translate/core';
 import { CookieProviderService } from 'src/app/core/services/cookie-provider.service';
 import { MixpanelService, MixpanelId } from './core/services/mixpanel.service';
 import { DOCUMENT } from '@angular/common';
-import { PwaRequestCatcherService } from './generated/services/pwa-request-catcher.service';
 import { PwaRequestPromptService } from './generated/services/pwa-request-prompt.service';
 
 @Component({
@@ -14,7 +13,7 @@ import { PwaRequestPromptService } from './generated/services/pwa-request-prompt
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterContentInit {
   title = 'SAKPaaS';
 
   locations$: Observable<Location[]>;
@@ -42,7 +41,9 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.locations$ = this.locationService.fetchLocations();
+  }
 
+  ngAfterContentInit() {
     const setTimeoutAsync = (a, t: number) => new Promise(() => setTimeout(a, t));
 
     setTimeoutAsync(() => this.pwaRequestPromptService.showPwaRequest(), 1000 * 30);
