7b2ceac5f652c51949e1c6e9ba8e4917ec6a8cbf
elsholz <hendrik.lankers.hl@googlemail.com>
2020-04-15 12:45:46 +0200

    remove unused import

--- SAKPaaS/src/app/generated/services.ts
+++ SAKPaaS/src/app/generated/services.ts
@@ -1,3 +1,2 @@
 export { LocationsService } from './services/locations.service';
 export { OccupancyService } from './services/occupancy.service';
-export { PwaRequestCatcherService } from '../core/services/pwa-request-catcher.service'
