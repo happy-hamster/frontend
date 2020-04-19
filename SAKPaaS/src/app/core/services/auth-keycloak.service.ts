f61e3204497f4df831a3a2316d913eaf2b92c2db
timgrohmann <timgrohmann@icloud.com>
2020-04-19 13:12:57 +0200

    added more detailed user info

--- SAKPaaS/src/app/core/services/auth-keycloak.service.ts
+++ SAKPaaS/src/app/core/services/auth-keycloak.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as Keycloak from 'keycloak-js';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { User } from '../models/user.interface';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -23,13 +23,17 @@ export class AuthKeycloakService {
   private loggedIn() {
     console.log('user logged in...');
     this.kcInstance.loadUserProfile().then(profile => {
-      console.log(profile);
-      this.user$.next({ username: profile.username, email: profile.email });
+      this.user$.next(User.fromKCProfile(profile));
     });
   }
 
   public login() {
     this.kcInstance.login().then(_ => this.loggedIn());
+  }
+
+  public logout() {
+    this.kcInstance.logout();
+    this.user$.next(null);
   }
 
   public register() {
