cdfeee6f937f5835a0ed1b595b78de7705a1beaf
timgrohmann <timgrohmann@icloud.com>
2020-04-22 15:47:09 +0200

    fixed the test case :D

--- SAKPaaS/src/app/core/services/permissions.service.spec.ts
+++ SAKPaaS/src/app/core/services/permissions.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PermissionsService } from './permissions.service';
+import { MatDialogTestingModule } from 'src/app/shared/mat-dialog-testing.module';
 
 describe('PermissionsService', () => {
   let service: PermissionsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        MatDialogTestingModule
+      ]
+    });
     service = TestBed.inject(PermissionsService);
   });
 
