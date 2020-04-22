c9cb6c1ebe08bd3a5d668e85bc16a08b67b15f9d
Niclas Kaufmann <niclaskau@web.de>
2020-04-22 22:10:04 +0200

    added function to redirect to legal sides

--- SAKPaaS/src/app/components/legal/legal-routing.module.ts
+++ SAKPaaS/src/app/components/legal/legal-routing.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LegalComponent } from 'src/app/components/legal/legal.component';
+import { ImpressumComponent } from 'src/app/components/legal/impressum/impressum.component';
+import { PrivacyPolicyComponent } from 'src/app/components/legal/privacy-policy/privacy-policy.component';
+import { CookieComponent } from 'src/app/components/legal/cookie/cookie.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: LegalComponent
+    redirectTo: 'impressum'
+  },
+  {
+    path: '',
+    component: LegalComponent,
+    children: [
+      {
+        path: 'impressum',
+        component: ImpressumComponent
+      },
+      {
+        path: 'privacy-policy',
+        component: PrivacyPolicyComponent
+      },
+      {
+        path: 'cookies',
+        component: CookieComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'impressum'
   }
 ];
 
