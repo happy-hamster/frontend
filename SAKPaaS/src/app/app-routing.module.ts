4478f83e6f59290ee717785c88868931b5738016
Niclas Kaufmann <niclaskau@web.de>
2020-04-22 21:17:23 +0200

    Enabled anchor scrolling globally

--- SAKPaaS/src/app/app-routing.module.ts
+++ SAKPaaS/src/app/app-routing.module.ts
@@ -13,7 +13,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+      anchorScrolling: 'enabled'
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
