294819cb8c8df956646c41d36814e68a2b1a5a4c
Unknown <lepathi@gmail.com>
2020-04-23 10:58:21 +0200

    Fix import
    after change requested by @nclskfm

--- SAKPaaS/src/app/components/footer/footer.module.ts
+++ SAKPaaS/src/app/components/footer/footer.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -10,7 +11,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
     FooterComponent
   ],
   imports: [
-    SharedModule
+    SharedModule,
+    MatIconModule
   ],
   exports: [
     FooterComponent
