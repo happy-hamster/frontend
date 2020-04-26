d8101458cdc3061f287613c79c7b50fbc3d89fbc
Unknown <lepathi@gmail.com>
2020-04-26 20:12:31 +0200

    Added interface
    for environments
    
    Fullfills wish by @nclskfm.

--- SAKPaaS/src/environments/environment.prod.ts
+++ SAKPaaS/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
-export const environment = {
+import { EnvironmentInterface } from './EnvironmentInterface';
+
+export const environment: EnvironmentInterface = {
   production: true,
   name: 'production',
   apiTarget: 'https://api.HappyHamster.org/v2'
