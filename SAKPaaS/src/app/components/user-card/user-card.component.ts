cf9402ab671167d72744df7410453877755f508f
Florian Redmann <Florian.Redmann@rewe-group.com>
2020-04-28 09:41:51 +0200

    TINF-284 | Show batch details
    - Made prettier

--- SAKPaaS/src/app/components/user-card/user-card.component.ts
+++ SAKPaaS/src/app/components/user-card/user-card.component.ts
@@ -13,18 +13,23 @@ export class UserCardComponent {
     public authService: AuthKeycloakService
   ) { }
 
-  change() {
+  async change() {
     const element = document.getElementById('user-card');
     if (this.expanded) {
-      element.classList.remove('expanded');
+      element.setAttribute('style', 'width: 12%;');
       document.getElementsByClassName('expand-icon')[0].classList.remove('turned');
       document.getElementsByClassName('content')[0].classList.remove('expandedContent');
     } else {
-      element.classList.add('expanded');
+      element.setAttribute('style', 'width: 25%;');
       document.getElementsByClassName('expand-icon')[0].classList.add('turned');
+      await this.delay(500);
       document.getElementsByClassName('content')[0].classList.add('expandedContent');
     }
     this.expanded = !this.expanded;
+  }
+
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
 }
