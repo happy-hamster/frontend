fd732308128a7c4dc3977b1899860a5a50485446
Luisa Fuss <luisa.fuss@web.de>
2020-04-28 13:44:38 +0200

    Arrows and Icons next to each other

--- SAKPaaS/src/app/components/user-card/user-card.component.ts
+++ SAKPaaS/src/app/components/user-card/user-card.component.ts
@@ -12,4 +12,18 @@ export class UserCardComponent {
     public authService: AuthKeycloakService
   ) { }
 
+  badges = [
+    {image: 'new_report.png', label: 'New Report'},
+    {image: 'next_level.png', label: 'Next Level'},
+    {image: 'trusted_shop.png', label: 'Trusted Shop'},
+    {image: 'week_1.png', label: 'Week 1'},
+    {image: 'trusted_user.png', label: 'Trusted User'},
+    {image: 'two_shops.png', label: 'Two Shops'},
+    {image: 'week_3.png', label: 'Week 3'},
+    {image: 'locked.png', label: 'Locked'},
+    {image: 'locked.png', label: 'Locked'},
+    {image: 'locked.png', label: 'Locked'}
+    ,
+  ];
+
 }
