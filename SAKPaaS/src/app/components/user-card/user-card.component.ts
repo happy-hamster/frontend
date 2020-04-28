b3f1d348b9e7e60d54bda910c5f9e7b92748ddea
timgrohmann <timgrohmann@icloud.com>
2020-04-28 18:57:34 +0200

    added translation and enums

--- SAKPaaS/src/app/components/user-card/user-card.component.ts
+++ SAKPaaS/src/app/components/user-card/user-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthKeycloakService } from 'src/app/core/services/auth-keycloak.service';
+import { BadgeType } from 'src/app/core/services/badge.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-user-card',
@@ -8,23 +10,13 @@ import { AuthKeycloakService } from 'src/app/core/services/auth-keycloak.service
 })
 export class UserCardComponent {
 
+  badges$: Observable<BadgeType[]>;
+
   constructor(
     public authService: AuthKeycloakService
-  ) { }
-
-  badges: Array<{image: string, label: string}> = [
-    {image: 'assets/badges/new_report.png', label: 'New Report'},
-    {image: 'assets/badges/next_level.png', label: 'Next Level'},
-    {image: 'assets/badges/trusted_shop.png', label: 'Trusted Shop'},
-    {image: 'assets/badges/week_1.png', label: 'Week 1'},
-    {image: 'assets/badges/trusted_user.png', label: 'Trusted User'},
-    {image: 'assets/badges/two_shops.png', label: 'Two Shops'},
-    {image: 'assets/badges/week_3.png', label: 'Week 3'},
-    {image: 'assets/badges/locked.png', label: 'Locked'},
-    {image: 'assets/badges/locked.png', label: 'Locked'},
-    {image: 'assets/badges/locked.png', label: 'Locked'}
-    ,
-  ];
+  ) {
+    this.badges$ = of([BadgeType.NEXT_LEVEL, BadgeType.WEEK_SPECIAL, BadgeType.TRUSTED_USER]);
+  }
 
   currentLevel = 'Novize';
   remainingReports = 1;
