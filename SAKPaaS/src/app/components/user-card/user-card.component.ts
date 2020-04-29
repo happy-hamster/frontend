66b64615b23843b36a019fc30d176dd37b330646
Florian Redmann <Florian.Redmann@rewe-group.com>
2020-04-29 10:28:38 +0200

    TINF-284 | Show batch details
    - Tried fixing wobbly behavior

--- SAKPaaS/src/app/components/user-card/user-card.component.ts
+++ SAKPaaS/src/app/components/user-card/user-card.component.ts
@@ -16,15 +16,14 @@ export class UserCardComponent {
   currentLevel = 'Novize';
   remainingReports = 1;
   currentProgress = 73;
-  async change() {
+
+  change() {
     const element = document.getElementById('user-card');
     if (this.expanded) {
       this.expanded = !this.expanded;
       element.setAttribute('style', 'width: 12%;');
       document.getElementsByClassName('expand-icon')[0].classList.remove('turned');
-      document.getElementsByClassName('content')[0].setAttribute('style', 'text-align: center;');
-      await this.delay(200);
-      document.getElementsByClassName('content')[0].removeAttribute('style');
+      document.getElementsByClassName('content')[0].setAttribute('style', 'text-align: left;');
 
 
     } else {
@@ -32,13 +31,7 @@ export class UserCardComponent {
       element.setAttribute('style', 'width: 25%;');
       document.getElementsByClassName('expand-icon')[0].classList.add('turned');
       document.getElementsByClassName('content')[0].setAttribute('style', 'text-align: center;');
-      await this.delay(200);
-      document.getElementsByClassName('content')[0].setAttribute('style', 'width: 90%; text-align: center;');
     }
-  }
-
-  delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
 }
