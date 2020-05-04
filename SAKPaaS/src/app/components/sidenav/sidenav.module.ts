e912b9f8e342fa82f619347032a4425f450d2963
timgrohmann <timgrohmann@icloud.com>
2020-05-04 09:40:35 +0200

    do not user ng-material component

--- SAKPaaS/src/app/components/sidenav/sidenav.module.ts
+++ SAKPaaS/src/app/components/sidenav/sidenav.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { SidenavComponent } from './sidenav.component';
 
@@ -9,8 +8,7 @@ import { SidenavComponent } from './sidenav.component';
 @NgModule({
   declarations: [SidenavComponent],
   imports: [
-    CommonModule,
-    MatSidenavModule
+    CommonModule
   ],
   exports: [SidenavComponent]
 })
