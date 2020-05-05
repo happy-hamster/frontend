2c3161e1ab3e007b4f6db301116b8ef8ea1c3208
patrickstecker <Stecker2000@gmail.com>
2020-05-05 15:19:44 +0200

    Changed favorites$ from Observable to BehaviorSubject

--- SAKPaaS/src/app/core/services/favorite.service.ts
+++ SAKPaaS/src/app/core/services/favorite.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 import {Location} from '../../generated/models/location';
 import {UserFavoritesService} from '../../generated/services/user-favorites.service';
-import {share} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoriteService {
 
-  favorites$ = new Observable<Location[]>(null);
+  favorites$ = new BehaviorSubject<Location[]>(null);
 
   constructor(
     private userFavoritesService: UserFavoritesService
   ) {
-    this.favorites$ = userFavoritesService.usersSelfFavoritesGet().pipe(share());
+    userFavoritesService.usersSelfFavoritesGet().subscribe(this.favorites$);
   }
 
-  getFavorites(): Observable<Location[]> {
+  getFavorites(): BehaviorSubject<Location[]> {
     return this.favorites$;
   }
 
