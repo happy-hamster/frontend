5e7ca1dda74dfae0975780a3dc93bf941f9f730f
nclskfm <kaufmann.niclas@web.de>
2020-05-06 18:43:21 +0200

    implemented http interceptor

---
+++ SAKPaaS/src/app/core/interceptors/api.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable, Provider, forwardRef } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, ReplaySubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { AuthKeycloakService } from '../services/auth-keycloak.service';
+
+export const API_INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useExisting: forwardRef(() => ApiInterceptor),
+  multi: true
+};
+
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor {
+  private token$ = new ReplaySubject<string>(1);
+
+  constructor(private authService: AuthKeycloakService) {
+    authService.getToken().subscribe(this.token$);
+  }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Apply the headers
+    return this.token$.pipe(switchMap(token => {
+      if (token) {
+        req = req.clone({
+          setHeaders: {
+            Authorization: 'Bearer ' + token
+          }
+        });
+      }
+      return next.handle(req);
+    }));
+
+  }
+}
