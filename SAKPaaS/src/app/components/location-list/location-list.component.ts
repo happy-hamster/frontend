371be4d561b87b90216a6b91046c65f9d4944a78
elsholz <hendrik.lankers.hl@googlemail.com>
2020-05-11 23:03:35 +0200

    reset location list to previous state

--- SAKPaaS/src/app/components/location-list/location-list.component.ts
+++ SAKPaaS/src/app/components/location-list/location-list.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {Location} from 'src/app/generated/models';
 import {GpsService} from '../../core/services/gps.service';
-import {getDistance as olGetDistance} from 'ol/sphere';
-import {LocationCardComponent} from "../location-card/location-card.component";
 
 @Component({
   selector: 'app-location-list',
@@ -16,12 +14,9 @@ export class LocationListComponent implements OnInit {
 
   constructor(
     private gpsService: GpsService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
-    this.locations.forEach((value, index, array) => {
-      value.distance$ = LocationCardComponent.toDistanceString(olGetDistance(this.gpsService.getCurrentGpsCoordinates().toArray(),
-        [value.coordinates.longitude, value.coordinates.latitude]));
-    });
   }
 }
