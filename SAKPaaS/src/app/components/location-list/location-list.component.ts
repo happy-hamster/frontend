cc814072be48f7824f7f6422be654f01a3c66a05
elsholz <hendrik.lankers.hl@googlemail.com>
2020-05-12 16:50:47 +0200

    sort data on every reload and on init

--- SAKPaaS/src/app/components/location-list/location-list.component.ts
+++ SAKPaaS/src/app/components/location-list/location-list.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, OnChanges} from '@angular/core';
 import {Location} from 'src/app/generated/models';
+import {getDistance as olGetDistance} from 'ol/sphere';
 import {GpsService} from '../../core/services/gps.service';
 
 @Component({
@@ -7,15 +8,39 @@ import {GpsService} from '../../core/services/gps.service';
   templateUrl: './location-list.component.html',
   styleUrls: ['./location-list.component.scss']
 })
-export class LocationListComponent implements OnInit {
+export class LocationListComponent implements OnInit, OnChanges {
 
   @Input() locations: Location[];
   @Input() isCloseToYouList?: boolean;
 
   constructor(
+    private gpsService: GpsService
   ) {
   }
 
   ngOnInit(): void {
+    this.sortLocationsClosest();
+  }
+
+  ngOnChanges(): void {
+    this.sortLocationsClosest();
+  }
+
+  private sortLocationsClosest(): void {
+    this.locations.sort((a: Location, b: Location) => {
+      const distA = olGetDistance(
+        [a.coordinates.longitude, a.coordinates.latitude], this.gpsService.getCurrentGpsCoordinates().toArray()
+      );
+      const distB = olGetDistance(
+        [b.coordinates.longitude, b.coordinates.latitude], this.gpsService.getCurrentGpsCoordinates().toArray()
+      );
+      console.log('sorting...' + this.locations.length);
+      return distA < distB ? -1 : 1;
+    });
+
+  }
+
+  public sortData(data) {
+    console.log(data);
   }
 }
