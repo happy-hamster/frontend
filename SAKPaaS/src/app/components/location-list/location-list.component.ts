cafa7850e4bf7762680eed3b19d57ac32cbbcdbd
elsholz <hendrik.lankers.hl@googlemail.com>
2020-05-14 11:46:15 +0200

    making requested changes

--- SAKPaaS/src/app/components/location-list/location-list.component.ts
+++ SAKPaaS/src/app/components/location-list/location-list.component.ts
@@ -1,7 +1,10 @@
-import { getDistance as olGetDistance } from 'ol/sphere';
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { Location } from 'src/app/generated/models';
-import { GpsService } from '../../core/services/gps.service';
+import {getDistance as olGetDistance} from 'ol/sphere';
+import {Component, OnInit, Input, OnChanges} from '@angular/core';
+import {Location} from 'src/app/generated/models';
+import {GpsService} from '../../core/services/gps.service';
+import {map} from "rxjs/operators";
+import {PositionCoordinates} from "../../core/models/position-coordinates.model";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-location-list',
@@ -12,34 +15,37 @@ export class LocationListComponent implements OnInit, OnChanges {
 
   @Input() locations: Location[];
   @Input() isCloseToYouList?: boolean;
+  private latestPosition: PositionCoordinates;
 
   constructor(
     private gpsService: GpsService
-  ) {  }
+  ) { }
 
   ngOnInit(): void {
+    this.gpsService.getGpsCoordinates().pipe(
+      map((position) => {
+        console.log('Got new position');
+        console.log(position);
+        this.sortLocationsClosest(position);
+        this.latestPosition = position;
+      })
+    );
   }
 
   ngOnChanges(): void {
-    this.sortLocationsClosest();
+    // reorder locations when the locations list changes because the search area changes
+    this.sortLocationsClosest(this.latestPosition);
   }
 
-  private sortLocationsClosest(): void {
-    this.locations.sort((a: Location, b: Location) => {
-      const actualGps = this.gpsService.getCurrentGpsCoordinates();
-      if (actualGps !== undefined) {
-        const actualPosition = actualGps.toArray();
-
-        const distA = olGetDistance([a.coordinates.longitude, a.coordinates.latitude], actualPosition);
-        const distB = olGetDistance([b.coordinates.longitude, b.coordinates.latitude], actualPosition);
+  private sortLocationsClosest(position: PositionCoordinates): void {
+    console.log('Sorting... Position:');
+    console.log(position);
+    if (position !== undefined) {
+      this.locations.sort((a: Location, b: Location) => {
+        const distA = olGetDistance([a.coordinates.longitude, a.coordinates.latitude], position.toArray());
+        const distB = olGetDistance([b.coordinates.longitude, b.coordinates.latitude], position.toArray());
         return distA < distB ? -1 : 1;
-      }
-      return 0;
-    });
-
-  }
-
-  public sortData(data) {
-    console.log(data);
+      });
+    }
   }
 }
